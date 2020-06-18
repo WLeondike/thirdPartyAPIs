7a15788c60a1c9a2eb8f46326b26b43bcf537a26
WLeondike <billyleondike@gmail.com>
2020-06-18 14:21:18 -0400

    homework startup

---
+++ script.js
@@ -0,0 +1,9 @@
+//TODOS
+//1. Display current time using Moment.js
+
+//2. Click event to save the text area content to local storage
+
+//3. When app opens, display content for each time block from local storage. 
+    // - it can be done with a loop (more efficient) or by targeting each time block separately
+
+//4. Change time block background color depending in the current hour (past, present, and future).
