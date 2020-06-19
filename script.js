f90853c7de71bdf41a2e3639585978a45ac694e2
WLeondike <billyleondike@gmail.com>
2020-06-19 00:15:34 -0400

    fixed some styling

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 //TODOS
 //1. Display current time using Moment.js
-$("#currentDay").text(moment().format("ddd MMM Do, YYYY"))
+$("#currentDay").text(moment().format("dddd, MMMM Do, YYYY"))
 moment().format("YYYY-MM-DD HH:mm");
 //2. Click event to save the text area content to local storage
-
+$("saveBtn").click(function(event){
+    localStorage.setItem("dayText", JSON.stringify(dayText));
+})
 //3. When app opens, display content for each time block from local storage. 
     // - it can be done with a loop (more efficient) or by targeting each time block separately
 
