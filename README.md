2b5350a59eb6db31106200d782532129a5e407f9
WLeondike <billyleondike@gmail.com>
2020-06-20 00:04:06 -0400

    Added some comments, updated README

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# thirdPartyAPIs
-This is a planner where you can add your activites per hour and save them to your local storage. We were referenced to moment.js to implement real time into our planner.
+# thirdPartyAPIs - Day Planner
+This is a planner where you can plan your work day per hour and save them to your local storage. We were referenced to moment.js to implement real time into our planner.
+
+moment.js is a third party API and this was our first assignment linking in an API and using such tool.
+
+The program really wasn't code heavy at all, and there really wasn't much JavaScript behind it. Although, the JavaScript behind this assignment was still pretty logic heavy and it took a few hours to work around some bugs. 
+
+## Inside this Planner:
+Once inside, you'll notice at the top that what I have built is a Work Day Scheduler. Typically, that consists of the hours between nine and five. If the time listed on the planner is before your current time, you'll see grey. The time block that represents your current time will show red. Any time in the future will then proceed to be green. You have the date displayed under the title, before the time blocks. Any text you input will be saved to your local storage divice by clicking the blue box with the save icon on the left hand side. The text you input will remain there until you clear out your storage inside the console log, or if you delete the contents and hit save once again. 
+
+### Here's what the planner looks like:
+
+<img src="./dayPlanner.png" alt = "Day Planner" width=600>
+
+Link to the planner: https://wleondike.github.io/thirdPartyAPIs/
+
+
--- script.js
+++ script.js
@@ -1,29 +1,20 @@
-// GLOBALS
-
-
-//TODOS
-//1. Display current time using Moment.js
+//Display current time using Moment.js
 $("#currentDay").text(moment().format("dddd, MMMM Do"))
 
-
-
-//2. Click event to save the text area content to local storage
-
-//3. When app opens, display content for each time block from local storage. 
-// - it can be done with a loop (more efficient) or by targeting each time block separately
-
-//4. Change time block background color depending in the current hour (past, present, and future).
-
+//Change time block background color depending in the current hour (past, present, and future).
 function timeBlock() {
     const presentTime = moment().hours();
     $(".row").each(function () {
         const plannerRow = parseInt($(this).attr("id"));
+        //if time listed is before current time color grey
         if (plannerRow < presentTime) {
             $(this).addClass("past");
         }
+        //if current time matches time listed, color red
         else if (plannerRow === presentTime) {
             $(this).removeClass("past");
             $(this).addClass("present");
+        //time after current time color green
         } else {
             $(this).removeClass("past");
             $(this).removeClass("present");
@@ -32,25 +23,25 @@ function timeBlock() {
     });
 }
 
-$("#9am .userInput").val(localStorage.getItem("9am"));
-$("#10am .userInput").val(localStorage.getItem("10am"));
-$("#11am .userInput").val(localStorage.getItem("11am"));
-$("#12pm .userInput").val(localStorage.getItem("12pm"));
-$("#1pm .userInput").val(localStorage.getItem("1pm"));
-$("#2pm .userInput").val(localStorage.getItem("2pm"));
-$("#3pm .userInput").val(localStorage.getItem("3pm"));
-$("#4pm .userInput").val(localStorage.getItem("4pm"));
-$("#5pm .userInput").val(localStorage.getItem("5pm"));
-$("#6pm .userInput").val(localStorage.getItem("6pm"));
-$("#7pm .userInput").val(localStorage.getItem("7pm"));
-$("#8pm .userInput").val(localStorage.getItem("8pm"));
-$("#9pm .userInput").val(localStorage.getItem("9pm"));
+//When app opens, display content for each time block from local storage. 
+// - it can be done with a loop (more efficient) or by targeting each time block separately
+$("#9 .userInput").val(localStorage.getItem("9"));
+$("#10 .userInput").val(localStorage.getItem("10"));
+$("#11 .userInput").val(localStorage.getItem("11"));
+$("#12 .userInput").val(localStorage.getItem("12"));
+$("#13 .userInput").val(localStorage.getItem("13"));
+$("#14 .userInput").val(localStorage.getItem("14"));
+$("#15 .userInput").val(localStorage.getItem("15"));
+$("#16 .userInput").val(localStorage.getItem("16"));
+$("#17 .userInput").val(localStorage.getItem("17"));
 
+
+//Click event to save the text area content to local storage
 $(document).ready(function(){
 
     $(".saved").on("click", function() {
         const value = $(this).siblings(".userInput").val();
-        const presentTime= $(this).parent().attr("id");
+        const presentTime = $(this).parent().attr("id");
         localStorage.setItem(presentTime, value);
     });
 
